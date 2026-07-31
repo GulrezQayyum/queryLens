0672f602ecf6af0bd588a4917fb698478bfc9aa6
gulrezqayyum <gulrezqayyum@gmail.com>
2026-07-31 15:58:32 +0500

    added configuration and corpus

---
+++ config.py
@@ -0,0 +1,12 @@
+EMBED_MODEL = "all-MiniLM-L6-v2"
+
+GROQ_MODEL = "llama-3.1-8b-instant"
+
+CHROMA_PATH = "./chroma_store"
+COLLECTION_NAME = "querylens_corpus"
+
+TOP_K = 5
+RRF_K = 60
+
+CHUNK_SIZE_WORDS = 80
+CHUNK_OVERLAP_WORDS = 15
